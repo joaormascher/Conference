7e599ef8f1e40b4e3dc5b74817774273d3d8bd47
Oliviajun <olivia.miura@gmail.com>
2016-02-29 10:51:03 -0300

    criei classe Presentation

---
+++ Conference/Conference/Models/Presentation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Conference.Models
+{
+    public class Presentation
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+        public string Presenter { get; set; }
+        public string Kind { get; set; }
+    }
+}
