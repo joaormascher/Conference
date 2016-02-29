3ae358b50dca69a90cc458fc344abbabd25e7529
Rodrigo Augusto de Carvalho <racarvalho1987@gmail.com>
2016-02-29 10:39:15 -0300

    Criação do Projeto/Pastas na Arquitetura MVC/Classe Presenter

---
+++ Conference/Conference/Models/Presenter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Conference.Models {
+    public class Presenter {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Title { get; set; }
+        public string Biography { get; set; }
+        public string UserOwner { get; set; }
+    }
+}
