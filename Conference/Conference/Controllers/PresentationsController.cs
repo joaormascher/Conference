5305e1a006a146902085d8a018146da5395fe25f
Oliviajun <olivia.miura@gmail.com>
2016-02-29 11:38:55 -0300

    criação controller PresentationsController

---
+++ Conference/Conference/Controllers/PresentationsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Conference.Models;
+
+namespace Conference.Controllers
+{
+    public class PresentationsController : Controller
+    {
+        private ConferenceContext db = new ConferenceContext();
+
+        // GET: Presentations
+        public ActionResult Index()
+        {
+            return View(db.Presentations.ToList());
+        }
+
+        // GET: Presentations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Presentation presentation = db.Presentations.Find(id);
+            if (presentation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(presentation);
+        }
+
+        // GET: Presentations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Presentations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Pid,Title,Abstract,Presenter,Kind")] Presentation presentation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Presentations.Add(presentation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(presentation);
+        }
+
+        // GET: Presentations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Presentation presentation = db.Presentations.Find(id);
+            if (presentation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(presentation);
+        }
+
+        // POST: Presentations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Pid,Title,Abstract,Presenter,Kind")] Presentation presentation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(presentation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(presentation);
+        }
+
+        // GET: Presentations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Presentation presentation = db.Presentations.Find(id);
+            if (presentation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(presentation);
+        }
+
+        // POST: Presentations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Presentation presentation = db.Presentations.Find(id);
+            db.Presentations.Remove(presentation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
