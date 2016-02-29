5577ff82e8f47921642b2e3b02bc3135c1bf2123
Rodrigo Augusto de Carvalho <racarvalho1987@gmail.com>
2016-02-29 11:17:33 -0300

    Criação do Controller para o Presenter

---
+++ Conference/Conference/Controllers/PresentersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Conference.Models;
+
+namespace Conference.Controllers
+{
+    public class PresentersController : Controller
+    {
+        private ConferenceContext db = new ConferenceContext();
+
+        // GET: Presenters
+        public ActionResult Index()
+        {
+            return View(db.Presenters.ToList());
+        }
+
+        // GET: Presenters/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Presenter presenter = db.Presenters.Find(id);
+            if (presenter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(presenter);
+        }
+
+        // GET: Presenters/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Presenters/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Email,Title,Biography,UserOwner")] Presenter presenter)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Presenters.Add(presenter);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(presenter);
+        }
+
+        // GET: Presenters/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Presenter presenter = db.Presenters.Find(id);
+            if (presenter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(presenter);
+        }
+
+        // POST: Presenters/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Email,Title,Biography,UserOwner")] Presenter presenter)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(presenter).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(presenter);
+        }
+
+        // GET: Presenters/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Presenter presenter = db.Presenters.Find(id);
+            if (presenter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(presenter);
+        }
+
+        // POST: Presenters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Presenter presenter = db.Presenters.Find(id);
+            db.Presenters.Remove(presenter);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Conference/Conference/Models/Presenter.cs
+++ Conference/Conference/Models/Presenter.cs
@@ -7,11 +7,11 @@ using System.Web;
 namespace Conference.Models {
     public class Presenter {
         [Key]
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Title { get; set; }
-        public string Biography { get; set; }
-        public string UserOwner { get; set; }
+        public int Id { get; set; } //Presenter ID Primary Key
+        public string Name { get; set; } //Presenter Name
+        public string Email { get; set; } //Presenter Email
+        public string Title { get; set; } //Presentation Title [Foreing Key]
+        public string Biography { get; set; }  //Presenter Biography
+        public string UserOwner { get; set; } //UserOwner (User who will manage the presentations
     }
 }
