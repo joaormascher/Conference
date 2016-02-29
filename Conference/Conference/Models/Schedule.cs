570abdea3773b342c7aa938894e73a9e6ea32442
gutierres <krolgutierres@gmail.com>
2016-02-29 11:10:38 -0300

    Criação classe Schedule

---
+++ Conference/Conference/Models/Schedule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Conference.Models
+{
+    public class Schedule
+    {
+        [Key]
+        public int IdSchedule { get; set; }
+        public DateTime DateHour { get; set; }
+        public string Host { get; set; }
+
+    }
+}
