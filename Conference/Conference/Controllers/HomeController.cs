0bea277cee3415a647980764d3b85ac96a2aba1c
pedro154 <pedro.ferreira154@gmail.com>
2016-03-03 14:23:03 -0300

    -

--- Conference/Conference/Controllers/HomeController.cs
+++ Conference/Conference/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Conference.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,42 @@ using System.Web.Mvc;
 
 namespace Conference.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         // GET: Home
         public ActionResult Index()
         {
+
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User u)
+        {
+            if (ModelState.IsValid)
+            {
+                using(ConferenceContext dc = new ConferenceContext())
+                {
+                    var v = dc.Users.Where(a=>a.Username.Equals(u.Username)&& a.Password.Equals(u.Password)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        Session["user"] = v;
+                        return RedirectToAction("Index");
+                        
+
+                    }
+                }
+            }
             return View();
         }
     }
+    
+
 }
