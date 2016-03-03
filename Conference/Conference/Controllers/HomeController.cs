ce384d22b6d3228ee45b8c3768b89f0c288ddee4
pedro154 <pedro.ferreira154@gmail.com>
2016-03-03 14:25:09 -0300

    -COMITANDO CERTO

--- Conference/Conference/Controllers/HomeController.cs
+++ Conference/Conference/Controllers/HomeController.cs
@@ -23,33 +23,7 @@ namespace Conference.Controllers
 
             return View();
         }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Login(User u)
-        {
-            if (ModelState.IsValid)
-            {
-                using(ConferenceContext dc = new ConferenceContext())
-                {
-                    var v = dc.Users.Where(a=>a.Username.Equals(u.Username)&& a.Password.Equals(u.Password)).FirstOrDefault();
-                    if (v != null)
-                    {
-                        Session["user"] = v;
-                        return RedirectToAction("Index");
-                        
-
-                    }
-                }
-            }
-            return View();
-        }
-
-        [AllowAnonymous]
-        public ActionResult Login(string returnURL)
-        {
-            return View();
-        }
-
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         [AllowAnonymous]
